420594d6a9f79522b3091099ec6ca5d4b79a71b2
Michael P Hartman <Michael.P.Hartman@Gmail.com>
2019-07-04 13:28:05 -0400

    Update Code.js

--- Code.js
+++ Code.js
@@ -1,1 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+  
+})
 
+
+
+audio.addEventListener('ended', (event) => {
+  console.log('audio stopped either because 1) it was over, ' + 'or 2) no further data is available.');
+  loadNext();
+})
+
+function loadNext() {
+  var audio = document.querySelector('audio')
+	audio.src = 'https://www.dropbox.com/s/r3pd176ivl80kvg/Barefoot%20Sunday%20Blues%20-%206%3A30%3A19%2C%204.06%20PM.mp3?dl=1'
+	audio.play()
+}
