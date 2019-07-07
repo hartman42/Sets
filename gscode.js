a6a825f81a29003581d4f15f656c2e03a0a0b909
Michael Hartman <michael.hartman@dunbararmored.com>
2019-07-06 22:05:53 -0400

    datetime stamp

--- gscode.js
+++ gscode.js
@@ -147,7 +147,7 @@ function firstTune() {
 		}
 	}
 
-	currentRow++;
+	getNextValidRow();
 
 	return getAudioFile();
 
@@ -159,5 +159,34 @@ function getAudioFile() {
 }
 
 function updateLastPlayed() {
+	var params = {
+        // The ID of the spreadsheet to update.
+        spreadsheetId: sheetID,  // TODO: Update placeholder value.
 
+        // The A1 notation of the values to update.
+        range: sheetRange,  // TODO: Update placeholder value.
+
+        // How the input data should be interpreted.
+        valueInputOption: 'RAW'  // TODO: Update placeholder value.
+	};
+
+	var d = new Date();
+	var s = (d.getMonth() + 1) + '/' + d.getDay() + '/' + d.getFullYear() + ' ' + d.getHours() + ':' + ('0' + d.getMinutes()).trimRight(2);
+
+	range.values[currentRow][range.values[0].indexOf('Last Played')] = s;
+
+	var valueRangeBody = {
+        // TODO: Add desired properties to the request body. All existing properties
+		// will be replaced.
+		values: range.values
+	};
+
+	var request = gapi.client.sheets.spreadsheets.values.update(params, valueRangeBody);
+	request.then(function(response) {
+        // TODO: Change code below to process the `response` object:
+        console.log(response.result);
+	}, function(reason) {
+        console.error('error: ' + reason.result.error.message);
+	});
 }
+
