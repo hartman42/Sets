877630df32e4ba102b0138392ae399db7b6d84b5
Michael Hartman <michael.hartman@dunbararmored.com>
2019-07-08 00:59:54 -0400

    Add Changes

--- code.js
+++ code.js
@@ -34,6 +34,48 @@ function setNextValidRow() {
 function loadNext() {
   audio.src = getFileName('Audio File');
   leadSheet.data = getFileName('Eb Lead Sheet') + '#view=FitV&toolbar=0&navpanes=0&scrollbar=0';
+  leadSheet.width = lsWidth;
+  leadSheet.height = lsHeight;
+  changesContainer.innerHTML = buildChangesTable();
+}
+
+function buildChangesTable() {
+	var tbl = '';
+	var iChanges = getChanges(iFldName);
+	var aChanges = getChanges(aFldName);
+	var bChanges = getChanges(bFldName);
+	var cChanges = getChanges(cFldName);
+	tbl = '<table>' + iChanges + aChanges + bChanges + cChanges + '</table>';
+	return tbl;
+}
+
+function getChanges(f) {
+	var c = range.values[currentRow][range.values[0].indexOf(f)];
+	if (c) {
+		var a = c.split(',');
+		c = '';
+		var m = range.values[currentRow][range.values[0].indexOf(meterField)];
+		for (var i = 0; i < a.length; i++) {
+			var s = '';
+			var e = '';
+			if (i%m==0) {
+				s += 'border-left:1px solid;';
+			}
+			if ((i+1)%(4*m)==0) {
+				s += 'border-right:1px solid;';
+				if ((i+1)!=a.length) {
+					e += '</tr><tr>';
+				}
+			}
+			if (s!='') {
+				s = ' style="' + s + '"';
+			}
+			c += '<td' + s + '>' + ((a[i]) ? a[i] : '&nbsp') + '</td>' + e;
+		}
+		return '<tr>' + c + '</tr>';
+	} else {
+		return '';
+	}
 }
 
 function loadFirstTune() {
@@ -59,8 +101,8 @@ function loadFirstTune() {
 	//get the next tune
 	setNextValidRow();
 
-	audio.src = getFileName('Audio File');
-	leadSheet.data = getFileName('Eb Lead Sheet') + '#view=FitV&toolbar=0&navpanes=0&scrollbar=0';
+	loadNext();
+
 }
 
 function getFileName(field) {
